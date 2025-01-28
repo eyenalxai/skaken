c59006907453957a692a9d3df2dcb8921e2f3f4a
Sax Dhiut <mail@iyxeyl.me>
2025-01-29 03:03:19 +0400

    test: Adjust bishop move test case FEN string

--- src/lib/moves.test.ts
+++ src/lib/moves.test.ts
@@ -101,7 +101,7 @@ describe("getValidMoves", () => {
 	describe("bishops", () => {
 		it("should generate bishop moves", () => {
 			expectMoves(
-				"rnbqkbnr/pppp1ppp/8/4p3/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+				"rnbqkbnr/pppp1ppp/8/4p3/8/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1",
 				"f1",
 				[
 					{ from: "f1", to: "e2" },
