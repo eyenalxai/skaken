2c899421f473bdd2d0d800289deb80fafa8416a3
Sax Dhiut <mail@iyxeyl.me>
2025-01-29 04:08:19 +0400

    test: Increase Vitest test timeout for complex scenarios

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,9 @@ import path from "node:path"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
+	test: {
+		testTimeout: 10000
+	},
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src")
