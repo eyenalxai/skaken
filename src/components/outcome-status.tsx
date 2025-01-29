305fdda17dfa0aea080c67c759bd8f28051aace9
Sax Dhiut <mail@iyxeyl.me>
2025-01-29 15:08:14 +0400

    feat: Add OutcomeStatus component to display game result

---
+++ src/components/outcome-status.tsx
@@ -0,0 +1,24 @@
+import type { GameOutcome } from "@/lib/types"
+import { cn } from "@/lib/utils"
+
+type OutcomeStatusProps = {
+	gameOutcome: GameOutcome | null
+}
+
+export const OutcomeStatus = ({ gameOutcome }: OutcomeStatusProps) => {
+	const outcomeText: Record<GameOutcome, string> = {
+		"threefold-repetition": "threefold repetition",
+		draw: "draw",
+		checkmate: "checkmate",
+		stalemate: "stalemate",
+		"insufficient-material": "insufficient material"
+	}
+
+	if (gameOutcome === null) return null
+
+	return (
+		<p className={cn("font-semibold", "text-red-500", "text-xl")}>
+			{outcomeText[gameOutcome]}
+		</p>
+	)
+}
